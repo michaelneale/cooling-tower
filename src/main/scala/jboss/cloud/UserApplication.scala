aea99ddc0661f883da879020b2ce7f1f8b84c226
Michael Neale <michaelneale@evenstar.local>
2009-10-10 17:35:02 +1100

    some saturday work

--- src/main/scala/jboss/cloud/UserApplication.scala
+++ src/main/scala/jboss/cloud/UserApplication.scala
@@ -1,10 +1,16 @@
 package jboss.cloud
 
 
-case class ApplicationRequirements(memory: Int, disk: Int, canCoExist: Boolean)
+case class ApplicationRequirements(memory: Int, disk: Int, canCoExist: Boolean, cpu: Int)
 case class Image(name: String, id: Int)
 case class Flavor(id: Int, memory: Int, storage: Int)
-case class Instance(id: Int, name: String, image: Image, flavor: Flavor)
+
+case class RunningApplication(requirements: ApplicationRequirements, version: Int, dateCreated: Long, dateLastUpdated: Long)
+
+case class Instance(id: Int, name: String, image: Image, flavor: Flavor, var applications: List[RunningApplication]) {
+  def getSpareMemory = flavor.memory - applications.foldLeft(0)(_ + _.requirements.memory)
+  def getSpareStorage = flavor.disk - applications.foldLeft(0)(_ + _.requirements.disk)
+}
 
 
 
--- src/test/scala/jboss/cloud/AssignmentTest.scala
+++ src/test/scala/jboss/cloud/AssignmentTest.scala
@@ -1,6 +1,9 @@
 package jboss.cloud
 
 
+import org.drools.builder.{KnowledgeBuilderFactory, ResourceType}
+import org.drools.io.ResourceFactory
+import org.drools.KnowledgeBaseFactory
 import org.testng.annotations.Test
 
 /**
@@ -13,13 +16,23 @@ class AssignmentTest {
 
   @Test def singleRunningInstance = {
 
-    val flv = Flavor(1, 256, 1024)
-    val img = Image("img", 2)
-    val instance = Instance(42, "mike", img, flv)
+    val kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder
+    kbuilder.add(ResourceFactory.newClassPathResource("server-assignment.drl"), ResourceType.DRL)
 
+    val kb = KnowledgeBaseFactory.newKnowledgeBase
+    kb.addKnowledgePackages(pkgs)
+
+
+    val img = Image("fedora11", 1)
+    val flv1 = Flavor(1, 256, 1024)
+    val flv2 = Flavor(2, 512, 1024)
+    val flv3 = Flavor(3, 1024, 10000)
+
+    val requirements = ApplicationRequirements(128, 256, 1)
+    val runningApplication = RunningApplication(ApplicationRequirements(128, 256, 1), 1, 10, 10)
     
 
-    
+
 
 
     println("ok")
