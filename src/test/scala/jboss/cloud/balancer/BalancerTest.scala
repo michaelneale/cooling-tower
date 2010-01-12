516758b9245c5bc53bff545e9c0a2c047fbc155b
Michael Neale <michaelneale@dhcp-128-191.syd.redhat.com>
2010-01-12 16:03:10 +1100

    Some more work on the planner - still not happy. punting.

--- src/test/scala/jboss/cloud/balancer/BalancerTest.scala
+++ src/test/scala/jboss/cloud/balancer/BalancerTest.scala
@@ -8,10 +8,9 @@ import org.testng.Assert._
 import org.scala_tools.javautils.Imports._
 
 /**
- * 
+ * Test the balance algorithm (which uses drools planner).
  * @author Michael Neale
  */
-
 class BalancerTest {
   @Test def initialSolution = {
 
@@ -52,6 +51,28 @@ class BalancerTest {
                             AppServerInstance(List(Application("foo"), Application("bar"))),
                             AppServerInstance(List(Application("foo"), Application("bar")))))).size)
 
+  }
+
+  @Test def bulkMove = {
+    val initialSolution = BalanceSolution(List(
+                          AppServerInstance(List(Application("foo"), Application("bar"))),
+                          AppServerInstance(List(Application("wee"), Application("waa")))))
+
+    val mm = new BulkMoveMaker
+    val list = mm.createCachedMoveList(initialSolution)
+    assertEquals(2, list.size)
+
+
+    assertTrue(BulkAppMove(AppServerInstance(List(Application("wah"))), AppServerInstance(List(Application("wee"))), List(Application("wee"))).isMoveDoable(null))
+    assertFalse(BulkAppMove(AppServerInstance(List(Application("wah"))), AppServerInstance(List(Application("baz"), Application("wah"))), List(Application("baz"), Application("wah"))).isMoveDoable(null))    
+
+
+    val as1 = AppServerInstance(List(Application("wah")))
+    val as2 = AppServerInstance(List(Application("wee")))
+    val move = BulkAppMove(as1, as2, List(Application("wee")))
+    move.moveApp
+    assertEquals(2, as1.apps.size)
+    assertEquals(0, as2.apps.size)
   }
 
 
@@ -119,5 +140,30 @@ class BalancerTest {
     
 
 
+  }
+
+  @Test def prettyLargeNumber = {
+
+    //every 10th server will have 2 apps on it
+    val list = (i: Int) => if (i % 10 == 0) List(Application("app1_" + i), Application("app2_" + i)) else List(Application("app_" + i))
+    val servers = (1 to 300).map(i => AppServerInstance(list(i))).toList
+
+    val initialSolution = BalanceSolution(servers)
+
+    val configurer = new XmlSolverConfigurer
+    configurer.configure("/jboss/cloud/balancer/plannerconf.xml");
+    val solver = configurer.buildSolver
+    solver.setStartingSolution(initialSolution)
+    solver.solve
+
+    val solution = solver.getBestSolution.asInstanceOf[BalanceSolution]
+    assertTrue(solution.applicationServers.filter(_.apps.size == 0).size > 0, solution.toString)
+
+    System.err.println("BEST SCORE:" + solver.getBestScore)
+    System.err.println("NUMBER OF FREE: " + solution.applicationServers.filter(_.apps.size == 0).size)
+
+
+
+    
   }
 }
--- src/test/scala/jboss/cloud/balancer/MoveMaker.scala
+++ src/test/scala/jboss/cloud/balancer/MoveMaker.scala
@@ -9,7 +9,7 @@ import reflect.BeanProperty
 import org.scala_tools.javautils.Imports._
 
 /**
- * The move factory for the planner/solver engine.
+ * The move factory for the planner/solver engine - suggests moves. 
  * @author Michael Neale
  */
 class MoveMaker extends CachedMoveFactory {
@@ -43,4 +43,36 @@ case class AppMove(@BeanProperty var target: AppServerInstance,
   def isMoveDoable(wm: WorkingMemory) = !target.apps.contains(app) && source.apps.contains(app)
 
   def createUndoMove(wm: WorkingMemory) = AppMove(source, target, app)
+}
+
+class BulkMoveMaker extends CachedMoveFactory {
+  def createCachedMoveList(sol: Solution) = {
+    val solution = sol.asInstanceOf[BalanceSolution]
+    val moves = for (source <- solution.applicationServers;
+                     target <- solution.applicationServers filter(_ != source))
+                yield (BulkAppMove(target, source, source.apps).asInstanceOf[Move])
+    moves.asJava
+  }
+}
+
+case class BulkAppMove(val target: AppServerInstance, val source: AppServerInstance, val appsToMove: List[Application]) extends Move {
+  def doMove(wm: WorkingMemory) = {
+    wm.retract(wm.getFactHandle(source))
+    wm.retract(wm.getFactHandle(target))
+    moveApp
+    wm.insert(source)
+    wm.insert(target)
+  }
+
+  def moveApp = {
+      source.apps = source.apps.remove(appsToMove.contains(_))
+      target.apps = appsToMove ++ target.apps
+  }
+
+  def createUndoMove(wm: WorkingMemory) = BulkAppMove(source, target, appsToMove)
+
+  def isMoveDoable(wm: WorkingMemory) = {
+    target.apps.filter(appsToMove.contains(_)).size == 0 &&
+    source.apps.filter(appsToMove.contains(_)).size == appsToMove.size
+  }
 }
