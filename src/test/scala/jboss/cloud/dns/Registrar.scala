feb22b66073a5f9380deb6865078b06ec03b41e8
Michael Neale <michaelneale@evenstar.local>
2010-01-18 20:37:38 +1100

    Lots more of dns api now working

--- src/test/scala/jboss/cloud/dns/Registrar.scala
+++ src/test/scala/jboss/cloud/dns/Registrar.scala
@@ -1,46 +1,24 @@
 package jboss.cloud.dns
 
 
-import java.io.File
+import java.io.{FileOutputStream, File}
 import java.net.InetAddress
 import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+import java.util.Date
+import org.apache.commons.io.IOUtils
 import org.xbill.DNS._
-/**
- * 
- * @author Michael Neale
- */
-
-trait NameRegistry {
-
-  def listDomains: Array[String]
-  def listSubDomains(domain: String): Array[String]
-
-  def registerNewDomain(domain: String, defaultAddress: String)
-  def changeDefaultAddress(domain: String, defaultAddress: String)
-
-  def bindSubDomain(domain: String, subDomain: String, address: String)
-  def aliasSubDomain(domain: String, subDomain: String, targetURI: String)
-
-
-  def currentAddress(domain: String) : String
-  def currentAddress(domain: String, subDomain: String) : String
-
-  /** TODO MX records separately. */
-}
+import org.scala_tools.javautils.Imports._
 
 /**
  * Manage the database of name registrations for the cloud servers
  * Pass in the root of the directory to hold the zone files, and the IP of the DNS server itself
+ * TODO: MX records? 
  */
-case class Registrar(rootDirectory: File, dnsServerAddress: String) extends NameRegistry {
+case class Registrar(rootDirectory: File, dnsServerAddress: String) {
 
   val TTL = 60
 
-  def listSubDomains(domain: String) = null
-
   def registerNewDomain(domain: String, defaultAddress: String) = {
-
     val adminEmailAddress = Name.fromString("admin." + domain, Name.root)
     val domainName = Name.fromString(domain, Name.root)
     val serial = new SimpleDateFormat("yyyymmdd").format(new Date).toInt
@@ -50,24 +28,57 @@ case class Registrar(rootDirectory: File, dnsServerAddress: String) extends Name
     val minimum = 60
     val defaultDNSName = Name.fromString("dns." + domain, Name.root)
     val soa = new SOARecord(domainName, DClass.IN, TTL, defaultDNSName, adminEmailAddress, serial, refresh, retry, expiry, minimum)
-
-
     val dnsRecord = new ARecord(defaultDNSName, DClass.IN, 2419200, InetAddress.getByName(dnsServerAddress))
     val nsEntry = new NSRecord(domainName, DClass.IN, 2419200, defaultDNSName)
     val zone = new Zone(domainName, Array[Record](soa, nsEntry, dnsRecord))
-    println("ZONE: \n" + zone.toMasterFile)
-    zone
+    if (defaultAddress != null && !defaultAddress.isEmpty)  zone.addRecord(new ARecord(domainName, DClass.IN, TTL, InetAddress.getByName(defaultAddress)))
+    IOUtils.write(zone.toMasterFile, new FileOutputStream(new File(rootDirectory, domain)))
+    zone.toMasterFile
   }
 
-  def changeDefaultAddress(domain: String, defaultAddress: String) = null
 
-  def currentAddress(domain: String) = null
+  def listSubDomains(domain: String) = {
+    val zone = loadZone(domain)
+    recordsFor(zone).filter(_.isInstanceOf[ARecord]).map(_.getName.toString).map(n => n.substring(0, n.length -1))
+  }
 
-  def bindSubDomain(domain: String, subDomain: String, address: String) = null
 
-  def listDomains = rootDirectory.listFiles.map(_.getName) 
+
+  /** Probably can have optional params for TTL etc... */
+  def bindSubDomain(domain: String, subDomain: String, address: String)  = {
+    //need to whip through RRset and Records to find the right one... then remove it, create a new one from it.
+    val zone = loadZone(domain)
+    val name = Name.fromString(subDomain + "." + domain, Name.root)
+    recordsFor(zone).find(_.getName == name).map(zone.removeRecord(_))
+    zone.addRecord(new ARecord(name, DClass.IN, TTL, InetAddress.getByName(address)))
+    IOUtils.write(zone.toMasterFile, new FileOutputStream(new File(rootDirectory, domain)))
+
+  }
 
   def aliasSubDomain(domain: String, subDomain: String, targetURI: String) = null
+  
+  def listDomains = rootDirectory.listFiles.map(_.getName) 
+
 
   def currentAddress(domain: String, subDomain: String) = null
+
+
+
+  private def loadZone(domain: String) : Zone = new Zone(Name.fromString(domain, Name.root), new File(rootDirectory, domain).getPath)
+
+  private def recordsFor(z: Zone) : Seq[Record] = {
+    val ls = new java.util.ArrayList[Record]
+    val it = z.iterator
+    while (it.hasNext) {
+      val rec = it.next.asInstanceOf[RRset]
+      val recs = rec.rrs
+      while(recs.hasNext) {
+        val rec = recs.next      
+        ls.add(rec.asInstanceOf[Record])
+      }
+    }
+    ls.asScala
+  }
+
+  
 }
--- src/test/scala/jboss/cloud/dns/RegistrarTest.scala
+++ src/test/scala/jboss/cloud/dns/RegistrarTest.scala
@@ -1,18 +1,51 @@
 package jboss.cloud.dns
 
 import java.io.File
-import org.testng.annotations.Test
+import org.testng.annotations.{BeforeTest, AfterSuite, BeforeSuite, Test}
 import org.testng.Assert._
+
 
 
 class RegistrarTest {
 
-  @Test def createNewZone = {
 
-    val reg = Registrar(new File("temp_test_zones"), "8.8.8.8")
-    println(reg.registerNewDomain("foo.com", "4.4.4.4"))
+  var zoneDirectory: File = new File("_test_zone_directory_")
+
+  @BeforeTest def directory = {
+    delete(zoneDirectory )
+    if (!zoneDirectory.exists) zoneDirectory.mkdir
+  }
+
+  @AfterSuite def cleanup = delete(zoneDirectory)
+
+  @Test def createNewZone = {
+    val reg = Registrar(zoneDirectory, "8.8.8.8")
+
+    assertEquals(0, reg.listDomains.size)
+    val zone = reg.registerNewDomain("samplezone.com", "4.4.4.4")
+    assertEquals(1, reg.listDomains.size)
+
+    var doms = reg.listSubDomains("samplezone.com")
+    assertEquals(doms.size, 2)
+    assertEquals("samplezone.com", doms(0))
+
+    reg.bindSubDomain("samplezone.com", "wee.samplezone.com", "1.1.1.1")
+    assertEquals(1, reg.listDomains.size)
+
+    doms = reg.listSubDomains("samplezone.com")
+    assertEquals(doms.size, 3)
+    doms.contains("wee.samplezone.com")
+    
 
   }
+
+
+
+
+  private def delete(f: File) : Unit = if (f.isDirectory) f.listFiles.map(delete) else f.delete
+
+
+
 
   
 }
