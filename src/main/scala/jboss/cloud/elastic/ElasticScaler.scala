9b7473b0b76dda79eed6f8776f8fdb91b2ee3247
Michael Neale <michaelneale@dhcp-128-191.syd.redhat.com>
2010-02-18 17:11:00 +1100

    fleshing out elastic scaling stuff

--- src/main/scala/jboss/cloud/elastic/ElasticScaler.scala
+++ src/main/scala/jboss/cloud/elastic/ElasticScaler.scala
@@ -3,15 +3,16 @@ package jboss.cloud.elastic
 /**
  * The elastic scaler which adds and removes nodes from an AS cluster.
  */
-class ElasticScaler
+class ElasticScaler {
 
+  def receiveMetric(name: String, value: Double, clusterNodeName: String) = {
+    
+  }
 
-case class AppCluster(id: String)
-case class AppServerInstance(id: String,
-                             busyConnectors: Int,
-                             heapUsage: Long,
-                             sessionCount: Int,
-                             cpuLoad: Int,
-                             server: Server,
-                             cluster: AppCluster)
-case class Server(id: String)
+}
+
+case class Cluster(name: String)
+case class Metric(node: ClusterNode, name: String, value: Double) //belongs to a cluster node
+case class ClusterNode(server: ServerInstance, cluster: Cluster, name: String) //belongs to cluster and a ServerInstance
+case class ServerInstance(name: String) 
+
